7886f7646ecb198873ba7954212ad6c7a1a3999f
atrcodebase <fyousufzai93@gmail.com>
2022-05-30 21:49:39 +0430

    rename KEY to key

--- app.R
+++ app.R
@@ -8,6 +8,12 @@ library(shinydashboard)
 # Custom function for comparing two data frames
 # source("https://raw.githubusercontent.com/Fahim-Ahmad/r_custom_functions/main/compare_df_function.R")
 compare_df <- function(df1, df2, unique_id_df1, unique_id_df2) {
+  
+  if ("KEY" %in% colnames(df1)) {
+    df1 <- df1 %>% 
+      rename(key = KEY)
+  }
+  
   df1 <- df1 %>% 
     select(KEY = all_of(unique_id_df1), everything()) %>% 
     mutate(across(-KEY, function(x)
@@ -15,6 +21,11 @@ compare_df <- function(df1, df2, unique_id_df1, unique_id_df2) {
     )) %>% 
     pivot_longer(-KEY, values_to = "value_1") %>% 
     mutate(value_1 = str_squish(value_1))
+  
+  if ("KEY" %in% colnames(df2)) {
+    df2 <- df2 %>% 
+      rename(key = KEY)
+  }
   
   df2 <- df2 %>% 
     select(KEY = all_of(unique_id_df2), everything()) %>% 
